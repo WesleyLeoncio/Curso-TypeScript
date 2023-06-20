c0308011fc6591ed2824cc5ecd515a014c04eb9b
Wesley Cruz <wesley.leoncio.izi@gmail.com>
2023-06-20 10:57:28 -0300

    class Negociações criada

--- typescript-curso-1/app/models/negociacoes.ts
+++ typescript-curso-1/app/models/negociacoes.ts
@@ -2,5 +2,14 @@ import { Negociacao } from "./negociacao.js";
 
 
 export class Negociacoes{
-    private negociacoes: Array<Negociacao>
+    private negociacoes: Array<Negociacao> = [];
+    
+    adiciona(negociacao: Negociacao):void{
+        this.negociacoes.push(negociacao);
+    }
+
+    lista(): Array<Negociacao>{
+        return this.negociacoes;
+    }
+
 }
